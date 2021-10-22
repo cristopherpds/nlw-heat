3c9ae06f04ac075d99ded5a56b762630ca89dbc0
Cristopher <josuepaiva7@gmail.com>
2021-10-22 09:00:13 -0300

    retiving user information from github API

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,16 +2,27 @@ import axios from "axios";
 /**
  * Recibir code(string)
  * Recuperar el acces_token en github
+ * Recuperar informacion del usuario en github
  * Verificar si el usuario existe en la DB
  * --si = generar token
  * --no = crear en la DB, y generar token
  * Retornar token con informacion del user
  */
+interface IAccessTokenResponse{
+  access_token: string;
+}
+
+interface IUserResponse{
+  avatar_url:string,
+  login: string,
+  id:number,
+  name: string
+}
 class AuthenticateUserService {
   async execute(code: string) {
-    const url = "https://github.com/login/oauth/acces_token"
+    const url = "https://github.com/login/oauth/access_token"
 
-    const res = await axios.post(url,null,{
+    const {data: accessTokenResponse} = await axios.post<IAccessTokenResponse>(url, null,{
       params:{
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
@@ -22,7 +33,13 @@ class AuthenticateUserService {
       }
     });
 
-    return res.data
+    const res = await axios.get<IUserResponse>("https://api.github.com/user",{
+      headers:{
+        authorization: `Bearer ${accessTokenResponse.access_token}`
+      }
+    });
+
+    return res.data;
   }
 }
 
