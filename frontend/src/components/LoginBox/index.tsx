7302ab0bc31a8f1b1e843f8a9fc15a5125d64078
Cristopher <josuepaiva7@gmail.com>
2021-10-24 16:08:08 -0300

    Create a context to signin and singOut

--- frontend/src/components/LoginBox/index.tsx
+++ frontend/src/components/LoginBox/index.tsx
@@ -1,10 +1,19 @@
 import styles from './styles.module.scss';
 import { VscGithubInverted } from 'react-icons/vsc';
+import { useContext } from 'react';
+import { AuthContext } from '../../context/auth';
+
+
+
 export function LoginBox() {
+
+  const { signInUrl, user } = useContext(AuthContext)
+  console.log(user);
+  
   return (
     <div className={styles.loginBoxWrapper}>
-      <strong>Entra y comparte tu mensaje</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <strong>Ingresa y comparte tu mensaje</strong>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24"/>
         Entrar con GitHub
       </a>
